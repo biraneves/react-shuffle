2b70736a6256fd7fed7f9367cd6ba8de5ce27c51
biraneves <ubirajara.neves@usp.br>
2022-08-19 11:55:10 -0300

    Criado o componente Main.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
     return (
         <div>
             <Header>Sorteio - Balboa</Header>
-            <main>
-            <div className="container mx-auto p-4">
-                <h2>O conteúdo fica aqui.</h2>
-            </div>
-            </main>
+            <Main>Conteúdo</Main>
         </div>
     )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main({
+    children,
+}) {
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+        {children}
+    </div>
+  );
+}
