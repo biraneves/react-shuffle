266b0862b7dde24b7e8ced8e361cb150881128d8
Ubirajara Neves <ubirajara.neves@usp.br>
2023-01-03 16:25:31 -0300

    Adaptação para 2023

--- src/data/people.js
+++ src/data/people.js
@@ -1,28 +1,32 @@
-import { getNewId } from "../services/idService"
+import { getNewId } from '../services/idService';
 
 export const people = [
     {
-        'name': 'Carol',
-        'photo': 'photos/carol.jpg',
+        name: 'Carol Dolescki',
+        photo: 'photos/carol_dolescki.jpg',
     },
     {
-        'name': 'Gabi',
-        'photo': 'photos/gabi.jpg',
+        name: 'Carol Valadão',
+        photo: 'photos/carol_valadao.jpg',
     },
     {
-        'name': 'Kazukas',
-        'photo': 'photos/kazukas.jpg',
+        name: 'Larissa',
+        photo: 'photos/larissa.jpg',
     },
     {
-        'name': 'Rayssa',
-        'photo': 'photos/rayssa.jpg',
+        name: 'Nicoll',
+        photo: 'photos/nicoll.jpg',
     },
     {
-        'name': 'Renata',
-        'photo': 'photos/renata.jpg',
+        name: 'Priscila',
+        photo: 'photos/priscila.jpg',
+    },
+    {
+        name: 'Renata',
+        photo: 'photos/renata.jpg',
     },
 ]
-    .map(item => {
-        return {id: getNewId(), ...item};
+    .map((item) => {
+        return { id: getNewId(), ...item };
     })
     .sort((a, b) => a.name.localeCompare(b.name));
